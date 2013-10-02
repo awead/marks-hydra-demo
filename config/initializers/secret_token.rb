d381b43471b951266d2bff3b936100ded5be0584
Mark Bussey <mark@curationexperts.com>
2013-10-01 21:22:45 -0400

    Initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HydraDemo::Application.config.secret_key_base = '66d2e802fd32e013f02331d6131a0bcd5943d09ecb39793d2147e7ff3ee7752e9b710544c38fb03b76806148c9c96fdaa8658bb3adcafc1706a52f423d302e8a'
