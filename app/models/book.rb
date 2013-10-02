837c9b2a46340af93b332f513e152ad13ba2cdcc
Mark Bussey <mark@curationexperts.com>
2013-10-02 09:06:13 -0400

    Created a book page model with relationship to the book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,6 @@ class Book < ActiveFedora::Base
   delegate :title, to: 'descMetadata', unique: true
   delegate :author, to: 'descMetadata', unique: true
 
+  has_many :pages, :property=> :is_part_of
+
 end
